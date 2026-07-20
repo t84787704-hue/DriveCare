94c412c33c9c10e3e698aeb559616778c31c1bdd
t84787704-hue <t84787704@gmail.com>
2026-07-20 23:14:46 +0300

    Create AddVehicleActivity.kt

---
+++ app/src/main/java/com/drivecare/app/AddVehicleActivity.kt
@@ -0,0 +1,12 @@
+package com.drivecare.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddVehicleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_vehicle)
+    }
+}
