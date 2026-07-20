07492c31e666ea0ed642e1392fff58102747f736
t84787704-hue <t84787704@gmail.com>
2026-07-20 23:54:31 +0300

    Create VehicleDetailsActivity.kt

---
+++ app/src/main/java/com/drivecare/app/VehicleDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.drivecare.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class VehicleDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vehicle_details)
+    }
+}
