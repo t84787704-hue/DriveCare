90e9defd9951d417a7b561a199cf2395dfdd1395
t84787704-hue <t84787704@gmail.com>
2026-07-20 23:20:21 +0300

    Update MainActivity.kt

--- app/src/main/java/com/drivecare/app/MainActivity.kt
+++ app/src/main/java/com/drivecare/app/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.drivecare.app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +10,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnMyVehicles = findViewById<Button>(R.id.btnMyVehicles)
+
+        btnMyVehicles.setOnClickListener {
+            val intent = Intent(this, AddVehicleActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
