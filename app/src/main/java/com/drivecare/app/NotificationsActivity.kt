1e8fe1bd1a98397dd9d504d2987b1e0544cd97cb
t84787704-hue <t84787704@gmail.com>
2026-07-21 18:23:00 +0300

    Update NotificationsActivity.kt

--- app/src/main/java/com/drivecare/app/NotificationsActivity.kt
+++ app/src/main/java/com/drivecare/app/NotificationsActivity.kt
@@ -1,17 +1,53 @@
 package com.drivecare.app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class NotificationsActivity : AppCompatActivity() {
 
+    private lateinit var tvNotificationRecord: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(
-            R.layout.activity_notifications
-        )
+        setContentView(R.layout.activity_notifications)
 
         title = "Notifications"
+
+        tvNotificationRecord =
+            findViewById(R.id.tvNotificationRecord)
+
+        findViewById<Button>(R.id.btnAddNotification)
+            .setOnClickListener {
+
+                startActivity(
+                    Intent(
+                        this,
+                        AddNotificationActivity::class.java
+                    )
+                )
+            }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val sharedPreferences =
+            getSharedPreferences(
+                "NotificationData",
+                MODE_PRIVATE
+            )
+
+        val notificationText =
+            sharedPreferences.getString(
+                "notification_text",
+                "No Notifications Added Yet."
+            )
+
+        tvNotificationRecord.text =
+            notificationText
     }
 }
