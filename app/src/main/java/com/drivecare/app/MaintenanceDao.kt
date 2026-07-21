949f86caa341c6a140303a37907c62a44b22161e
t84787704-hue <t84787704@gmail.com>
2026-07-21 14:25:02 +0300

    Create MaintenanceDao.kt

---
+++ app/src/main/java/com/drivecare/app/MaintenanceDao.kt
@@ -0,0 +1,47 @@
+package com.drivecare.app
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface MaintenanceDao {
+
+    @Insert
+    suspend fun insertMaintenance(
+        maintenance: Maintenance
+    )
+
+    @Update
+    suspend fun updateMaintenance(
+        maintenance: Maintenance
+    )
+
+    @Delete
+    suspend fun deleteMaintenance(
+        maintenance: Maintenance
+    )
+
+    @Query(
+        "SELECT * FROM maintenance_history " +
+                "ORDER BY createdAt DESC"
+    )
+    suspend fun getAllMaintenance():
+            List<Maintenance>
+
+    @Query(
+        "SELECT * FROM maintenance_history " +
+                "WHERE id = :id"
+    )
+    suspend fun getMaintenanceById(
+        id: Int
+    ): Maintenance?
+
+    @Query(
+        "DELETE FROM maintenance_history"
+    )
+    suspend fun deleteAllMaintenance()
+
+}
