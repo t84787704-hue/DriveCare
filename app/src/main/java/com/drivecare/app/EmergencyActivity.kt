d9c7c08657df0adce426b5c0d13af97afac9b23d
t84787704-hue <t84787704@gmail.com>
2026-07-21 18:10:35 +0300

    Update EmergencyActivity.kt

--- app/src/main/java/com/drivecare/app/EmergencyActivity.kt
+++ app/src/main/java/com/drivecare/app/EmergencyActivity.kt
@@ -1,17 +1,53 @@
 package com.drivecare.app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class EmergencyActivity : AppCompatActivity() {
 
+    private lateinit var tvEmergencyRecord: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(
-            R.layout.activity_emergency
-        )
+        setContentView(R.layout.activity_emergency)
 
         title = "Emergency Contacts"
+
+        tvEmergencyRecord =
+            findViewById(R.id.tvEmergencyRecord)
+
+        findViewById<Button>(R.id.btnAddEmergency)
+            .setOnClickListener {
+
+                startActivity(
+                    Intent(
+                        this,
+                        AddEmergencyActivity::class.java
+                    )
+                )
+            }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val sharedPreferences =
+            getSharedPreferences(
+                "EmergencyData",
+                MODE_PRIVATE
+            )
+
+        val emergencyContact =
+            sharedPreferences.getString(
+                "emergency_contact",
+                "No Emergency Contacts Added Yet."
+            )
+
+        tvEmergencyRecord.text =
+            emergencyContact
     }
 }
