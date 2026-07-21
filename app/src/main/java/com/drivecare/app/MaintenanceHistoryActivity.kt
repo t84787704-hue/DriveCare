69ba51941fe805c0632e88639527f220a53ebec7
t84787704-hue <t84787704@gmail.com>
2026-07-21 16:20:52 +0300

    Create MaintenanceHistoryActivity.kt

---
+++ app/src/main/java/com/drivecare/app/MaintenanceHistoryActivity.kt
@@ -0,0 +1,13 @@
+package com.drivecare.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MaintenanceHistoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        title = "Maintenance History"
+    }
+}
