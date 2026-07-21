ed7abe8fdab880d6614c57d1760ad4c1df8f553a
t84787704-hue <t84787704@gmail.com>
2026-07-21 21:06:24 +0300

    Create MaintenanceAdapter.kt

---
+++ app/src/main/java/com/drivecare/app/MaintenanceAdapter.kt
@@ -0,0 +1,63 @@
+package com.drivecare.app
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MaintenanceAdapter(
+    private val maintenanceList: List<Maintenance>
+) : RecyclerView.Adapter<MaintenanceAdapter.MaintenanceViewHolder>() {
+
+    class MaintenanceViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+
+        val tvServiceTitle: TextView =
+            itemView.findViewById(R.id.tvServiceTitle)
+
+        val tvServiceType: TextView =
+            itemView.findViewById(R.id.tvServiceType)
+
+        val tvServiceDate: TextView =
+            itemView.findViewById(R.id.tvServiceDate)
+
+        val tvMaintenanceDetails: TextView =
+            itemView.findViewById(R.id.tvMaintenanceDetails)
+
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MaintenanceViewHolder {
+
+        val view = LayoutInflater.from(
+            parent.context
+        ).inflate(
+            R.layout.item_maintenance,
+            parent,
+            false
+        )
+
+        return MaintenanceViewHolder(view)
+    }
+
+    override fun onBindViewHolder(
+        holder: MaintenanceViewHolder,
+        position: Int
+    ) {
+
+        val maintenance =
+            maintenanceList[position]
+
+        holder.tvServiceTitle.text =
+            maintenance.serviceTitle
+
+        holder.tvServiceType.text =
+            "Service Type : ${maintenance.serviceType}"
+
+        holder.tvServiceDate.text =
+            "Service Date : ${maintenance.serviceDate}"
+
+        holder.tvMaintenanceDetails
