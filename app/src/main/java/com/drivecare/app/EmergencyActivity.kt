e2650eef169da9c52f801d88e1b57c3848365bb5
t84787704-hue <t84787704@gmail.com>
2026-07-21 16:36:49 +0300

    Create EmergencyActivity.kt

---
+++ app/src/main/java/com/drivecare/app/EmergencyActivity.kt
@@ -0,0 +1,17 @@
+package com.drivecare.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EmergencyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(
+            R.layout.activity_emergency
+        )
+
+        title = "Emergency Contacts"
+    }
+}
