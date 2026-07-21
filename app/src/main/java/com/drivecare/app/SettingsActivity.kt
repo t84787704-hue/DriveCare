6a58722a20a2df53e480980f03537eba2d87c2a3
t84787704-hue <t84787704@gmail.com>
2026-07-21 18:34:45 +0300

    Update SettingsActivity.kt

--- app/src/main/java/com/drivecare/app/SettingsActivity.kt
+++ app/src/main/java/com/drivecare/app/SettingsActivity.kt
@@ -1,17 +1,53 @@
 package com.drivecare.app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
 
+    private lateinit var tvSettingRecord: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(
-            R.layout.activity_settings
-        )
+        setContentView(R.layout.activity_settings)
 
         title = "Settings"
+
+        tvSettingRecord =
+            findViewById(R.id.tvSettingRecord)
+
+        findViewById<Button>(R.id.btnAddSetting)
+            .setOnClickListener {
+
+                startActivity(
+                    Intent(
+                        this,
+                        AddSettingsActivity::class.java
+                    )
+                )
+            }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val sharedPreferences =
+            getSharedPreferences(
+                "SettingsData",
+                MODE_PRIVATE
+            )
+
+        val settingText =
+            sharedPreferences.getString(
+                "setting_text",
+                "No Settings Added Yet."
+            )
+
+        tvSettingRecord.text =
+            settingText
     }
 }
