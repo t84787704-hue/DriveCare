ef7521fe5f0621c0c0a1eb2620fec742d3774518
t84787704-hue <t84787704@gmail.com>
2026-07-21 16:47:13 +0300

    Create SettingsActivity.kt

---
+++ app/src/main/java/com/drivecare/app/SettingsActivity.kt
@@ -0,0 +1,17 @@
+package com.drivecare.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(
+            R.layout.activity_settings
+        )
+
+        title = "Settings"
+    }
+}
